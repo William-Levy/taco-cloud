8cb7a1098aaf96b8dda2e6fb08052ce45cdb00a6
William Levy <w_levy@outlook.com>
2023-12-11 16:56:18 -0500

    initialized home controller

---
+++ src/main/java/wl/tacocloud/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package wl.tacocloud.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(@RequestParam String param) {
+      return "home";
+    }
+    
+}
