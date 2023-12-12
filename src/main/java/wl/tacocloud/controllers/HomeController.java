02d4b4546c944c5642b6616b080fd38fd2ce4165
William Levy <w_levy@outlook.com>
2023-12-12 16:27:13 -0500

    fix issue with controller test failing

--- src/main/java/wl/tacocloud/controllers/HomeController.java
+++ src/main/java/wl/tacocloud/controllers/HomeController.java
@@ -2,14 +2,13 @@ package wl.tacocloud.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home(@RequestParam String param) {
+    public String home() {
       return "home";
     }
     
